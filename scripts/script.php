5706d8647a759091a0c236b61dae106ccdff2d2c
Thalles Sobral <thazsobral@gmail.com>
2020-05-21 10:21:11 -0300

    serparei as funções

--- scripts/script.php
+++ scripts/script.php
@@ -1,12 +1,26 @@
 <?php
-  $servidor = "localhost";
-  $usuario = "root";
-  $senha = "";
-  $nome_bd = "test";
-
-  $conn = new mysqli($servidor, $usuario, $senha, $nome_bd);
-  if ($conn) {
-    $sql = "SELECT * FROM usuario";
+  $server = "localhost";
+  $userDb = "root";
+  $passwordDb = "";
+  $nameDb = "cadastro";
+  
+  try {
+    // adicionar as funções para excução
+  } catch (Exception $e) {
+    // tratamento de exceções
+  }
+  // funções
+  function connectDb ($server, $user, $password, $nameDb) {
+    $connection = new mysqli($server, $user, $password, $nameDb);
+    if ($connection) {
+      return $connection
+    } else {
+      return throw new Exception("Database not connected");
+    }
+  }
+  
+  function getAllDataTable ($nameTable) {
+    $sql = "SELECT * FROM $nameTable";
     $resultado = $conn->query($sql);
     if ($resultado)
       while ($row =  mysqli_fetch_array( $resultado) ) {
@@ -16,14 +30,9 @@
       echo "Erro ao executar: " . $conn->error;
 
     $conn->close();
-
-  } else {
-    die("Não conectado".mysql_error());
   }
 
-
-  // funções
-  function inserNewData () {
+  function inserNewData ($nameTable, $name, $email, $password) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -39,8 +48,8 @@
     $conn->close();
   }
 
-  function createTable () {
-    $sql = "CREATE TABLE usuario (
+  function createTable ($nameTable) {
+    $sql = "CREATE TABLE $nameTable (
       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
       nome VARCHAR (30) NOT NULL,
       email VARCHAR (30) NOT NULL,
@@ -55,8 +64,8 @@
     $conn->close();
   }
 
-  function createDb () {
-    $sql = "CREATE DATABASE cadastro";
+  function createDb ($nameDb) {
+    $sql = "CREATE DATABASE $nameDb";
     if ($conn-> query($sql) === TRUE) {
       echo "Database created successfully";
     } else {
